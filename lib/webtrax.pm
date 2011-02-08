193c1fcca7902bad68a54089907b618a970e16d3
ben hengst <notbenh@cpan.org>
2011-02-07 20:32:14 -0800

    broad sketch

--- lib/webtrax.pm
+++ lib/webtrax.pm
@@ -3,8 +3,46 @@ use Dancer ':syntax';
 
 our $VERSION = '0.1';
 
+# force login
+before sub {
+   if (! session('user') && request->path_info !~ m{^/login}) {
+      var requested_path => request->path_info;
+      request->path_info('/login');
+   }
+};
+
 get '/' => sub {
     template 'index';
 };
+
+get '/login' => sub {
+   # Display a login page; the original URL they requested is available as
+   # vars->{requested_path}, so could be put in a hidden field in the form
+   template 'login', { path => vars->{requested_path} };
+};
+
+post '/login' => sub {
+   # Validate the username and password they supplied
+   if (params->{user} eq 'bob' && params->{pass} eq 'letmein') {
+      session user => params->{user};
+      redirect params->{path} || '/';
+   } else {
+      redirect '/login?failed=1';
+   }
+};
+
+
+get '/roles' => sub{
+   template 'roles';
+};
+
+get '/goals' => sub{
+   template 'goals';
+};
+
+get '/tasks' => sub{
+   template 'tasks';
+};
+
 
 true;
